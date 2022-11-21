95413353c386231de660e17e288cf9fe2d4d6beb
Emma <em0795@yandex.ru>
2022-11-21 22:36:42 +0400

    add DZ34

---
+++ DZ34/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] InitArray(int n)
+{
+    int[] result = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = rnd.Next(100,1000);
+    }
+    return result;
+}
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {   
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Write("Введите длину массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = InitArray(n);
+Print(array);
+int count =0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    count++;
+    {
+        Console.Write($"Из {array.Length} чисел, {count} четные");
+        break;
+    }
+}
